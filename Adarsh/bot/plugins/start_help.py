2128ac939b766fabd2e87c10b70d28b81e83d850
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-01-29 12:27:33 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -128,7 +128,7 @@ async def help_handler(bot, message):
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("💁‍♂️ Owner", url="https://t.me/Acckerman_offical)],
+                [InlineKeyboardButton("💁‍♂️ Owner", url="https://t.me/Acckerman_offical")],
                 [InlineKeyboardButton("💥 creater", url="https://t.me/Acckerman_offical")]
             ]
         )
